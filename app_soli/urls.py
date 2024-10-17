bc0daf8d4814b6bfa065709317ae3ea192519f9a
Marina Hoffmann <mhg@cesar.school>
2024-10-17 17:51:27 -0300

    adicionando pag procurar poor linha

--- app_soli/urls.py
+++ app_soli/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('excluir/<int:lembrete_id>/', views.excluir_lembrete, name='excluir_lembrete'),  # Excluir lembrete
     path('excluir_cultura/<int:cultura_id>/', views.excluir_cultura, name='excluir_cultura'),  # Excluir cultura
     path('weather/', views.weather, name='weather'),  # Página para o clima
+    path('procurarlinhas/', views.procurar_linhas_view, name='procurarlinha'), #procurando por linhas
 ]
