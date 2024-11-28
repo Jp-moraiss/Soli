50c76189dfd9210064a45b8a45476a9afb4906eb
brunooassuncao <bas@cesar.school>
2024-11-28 02:26:20 -0300

    testes automatizados

---
+++ cypress/e2e/historico.cy.js
@@ -0,0 +1,43 @@
+describe("teste 'meu histórico'", () => {
+    const cleanupAndSetupData = () => {
+        cy.request('GET', 'http://127.0.0.1:8000/cleanup_db/').then(() => {
+            cy.clearCookies(); // Limpa cookies
+            cy.clearLocalStorage(); // Limpa o localStorage
+            cy.visit('http://127.0.0.1:8000/'); // Visitar a URL inicial
+        });
+    };
+
+    const registerUser = () => {
+        cy.get('.register-link').click();
+        cy.get('#username').type('cesar.school');
+        cy.get('#email').type("bas@cesar.school");
+        cy.get('#password').type("1234");
+        cy.get('#confirm-password').type("1234");
+        cy.get('.submit-button').click();
+    };
+
+    beforeEach(() => {
+        cleanupAndSetupData(); // Limpa e prepara o banco de dados antes de cada teste
+    });
+
+    it('cenario1 - busca encontrada', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('.menu > a > svg').click();
+        cy.get('#searchInput').type("tomate");
+        cy.wait(2000);
+    });
+
+    it('cenario2 - busca não encontrada', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('.menu > a > svg').click();
+        cy.get('#searchInput').type("brocolis");
+        cy.wait(2000);
+    });
+
+});
