ece61edcd72d333cee976f5153abe13c82a6f9fe
brunooassuncao <bas@cesar.school>
2024-11-28 20:34:34 -0300

    testes

--- cypress/e2e/gerenciar_culturas.cy.js
+++ cypress/e2e/gerenciar_culturas.cy.js
@@ -1,113 +1,150 @@
-describe('Teste da historia 6', () => {
-   
-  const cleanupAndSetupData = () => {
-    //login
-    cy.visit('http://127.0.0.1:8000/');
-    cy.get('#username').type('laviniaxs');
-    cy.get('#password').type('1234');
-    cy.get('.submit-button').click();
-    cy.wait(3000);
-   
-    cy.visit('http://127.0.0.1:8000/verculturas/');
-    cy.wait(1000);
+describe("teste 'achar por linha'", () => {
+    const cleanupAndSetupData = () => {
+        cy.request('GET', 'http://127.0.0.1:8000/cleanup_db/').then(() => {
+            cy.clearCookies(); // Limpa cookies
+            cy.clearLocalStorage(); // Limpa o localStorage
+            cy.visit('http://127.0.0.1:8000/'); // Visitar a URL inicial
+        });
+    };
 
-    // editando cultura
-    cy.get('#cultura-20 > .editar-btn > .fas').click();
-    cy.wait(5000);
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-area').clear();
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-area').type('Área: c');
-    cy.wait(5000);
-    cy.get('#cultura-20 > .confirmar-edicao > .fas').click();
-    cy.wait(5000);
-  };
+    const registerUser = () => {
+        cy.get('.register-link').click();
+        cy.get('#username').type('cesar.school');
+        cy.get('#email').type("bas@cesar.school");
+        cy.get('#password').type("1234");
+        cy.get('#confirm-password').type("1234");
+        cy.get('.submit-button').click();
+    };
 
-  it('cenario1 - mudando area; 100%', () => {
-      cleanupAndSetupData();
-  });
+    // Captura exceções não tratadas
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        console.error('Erro não tratado detectado:', err);
+        return false; // Evita falhas automáticas nos testes
+    });
 
+    beforeEach(() => {
+        cleanupAndSetupData(); // Limpa e prepara o banco de dados antes de cada teste
+    });
 
-  it('cenario2 - mudando linha; 100%', () => {
-    //login
-    cy.visit('http://127.0.0.1:8000/');
-    cy.get('#username').type('laviniaxs');
-    cy.get('#password').type('1234');
-    cy.get('.submit-button').click();
-    cy.wait(3000);
-   
-    cy.visit('http://127.0.0.1:8000/verculturas/');
-    cy.wait(1000);
+    it('cenario1 - alterar a área', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('[href="/add/"]').click();
+        cy.get('.input_cultura').select('Tomate');
+        cy.get('[name="area"]').select('C');
+        cy.get('[name="linha"]').select('3');
+        cy.get('.container_loc-desc > .span > input').type('Sementes de Daniel');
+        cy.get('#data_plantio').type('2024-11-23');
+        cy.get('#duracao').type('4');
+        cy.get('#unidade_duracao').select('meses');
+        cy.get(':nth-child(2) > .atv > .input_number').type('10');
+        cy.get(':nth-child(2) > .atv > .select_atv').select('dias');
+        cy.get(':nth-child(3) > .atv > .input_number').type('1');
+        cy.get(':nth-child(3) > .atv > .select_atv').select('semanas');
+        cy.get('.registrar_cultura').click();
+        cy.wait(2000);
+        cy.get('[href="/verculturas/"]').click();  
+        cy.get('.editar-btn > .fas').first().click(); 
+        cy.wait(2000);
+        cy.get('.cultura-area').contains('Área: C').clear().type('Área: B');
+        cy.wait(2000);       
+        cy.get('.confirmar-edicao > .fas').first().click();
+        cy.wait(2000);
+    });
 
-    // editando cultura
-    cy.get('#cultura-20 > .editar-btn > .fas').click();
-    cy.wait(5000);
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-area').clear();
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-area').type('Área: c');
-    cy.wait(5000);
-    cy.get('#cultura-20 > .confirmar-edicao > .fas').click();
-    cy.wait(5000);
-  });
+    it('cenario2 - alterar as linhas', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('[href="/add/"]').click();
+        cy.get('.input_cultura').select('Tomate');
+        cy.get('[name="area"]').select('C');
+        cy.get('[name="linha"]').select('3');
+        cy.get('.container_loc-desc > .span > input').type('Sementes de Daniel');
+        cy.get('#data_plantio').type('2024-11-23');
+        cy.get('#duracao').type('4');
+        cy.get('#unidade_duracao').select('meses');
+        cy.get(':nth-child(2) > .atv > .input_number').type('10');
+        cy.get(':nth-child(2) > .atv > .select_atv').select('dias');
+        cy.get(':nth-child(3) > .atv > .input_number').type('1');
+        cy.get(':nth-child(3) > .atv > .select_atv').select('semanas');
+        cy.get('.registrar_cultura').click();
+        cy.wait(2000);
+        cy.get('[href="/verculturas/"]').click();  
+        cy.get('.editar-btn > .fas').first().click(); 
+        cy.wait(2000);
+        cy.get('.cultura-linha').contains('Linhas: 3').clear().type('Linhas: 2');
+        cy.wait(2000);       
+        cy.get('.confirmar-edicao > .fas').first().click();
+        cy.wait(2000);
+    });
 
-  it('cenario3 - mudando data plantio; erro', () => { // contagem dos dias nao vai alterar
-    //login
-    cy.visit('http://127.0.0.1:8000/');
-    cy.get('#username').type('laviniaxs');
-    cy.get('#password').type('1234');
-    cy.get('.submit-button').click();
-    cy.wait(3000);
-   
-    cy.visit('http://127.0.0.1:8000/verculturas/');
-    cy.wait(1000);
+    it('cenario3 - alterar data de plantio', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('[href="/add/"]').click();
+        cy.get('.input_cultura').select('Tomate');
+        cy.get('[name="area"]').select('C');
+        cy.get('[name="linha"]').select('3');
+        cy.get('.container_loc-desc > .span > input').type('Sementes de Daniel');
+        cy.get('#data_plantio').type('2024-11-23');
+        cy.get('#duracao').type('4');
+        cy.get('#unidade_duracao').select('meses');
+        cy.get(':nth-child(2) > .atv > .input_number').type('10');
+        cy.get(':nth-child(2) > .atv > .select_atv').select('dias');
+        cy.get(':nth-child(3) > .atv > .input_number').type('1');
+        cy.get(':nth-child(3) > .atv > .select_atv').select('semanas');
+        cy.get('.registrar_cultura').click();
+        cy.wait(2000);
+        cy.get('[href="/verculturas/"]').click();  
+        cy.get('.editar-btn > .fas').first().click(); 
+        cy.wait(2000);
+        cy.get('.data-plantio')
+        .clear()
+        .type('2024-11-28')
+        .trigger('change');
+        cy.wait(2000);       
+        cy.get('.confirmar-edicao > .fas').first().click();
+        cy.wait(2000);
+    });
 
-    // editando cultura
-    cy.get('#cultura-20 > .editar-btn > .fas').click();
-    cy.wait(5000);
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-data-plantio > .data-plantio').click();
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-data-plantio > .data-plantio').clear();
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-data-plantio > .data-plantio').type('2024-10-23');
-    cy.wait(5000);
-    cy.get('#cultura-20 > .confirmar-edicao > .fas').click();
-    cy.wait(5000);
-  });
+    it('cenario4 - alterar data de colheita', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('[href="/add/"]').click();
+        cy.get('.input_cultura').select('Tomate');
+        cy.get('[name="area"]').select('C');
+        cy.get('[name="linha"]').select('3');
+        cy.get('.container_loc-desc > .span > input').type('Sementes de Daniel');
+        cy.get('#data_plantio').type('2024-11-23');
+        cy.get('#duracao').type('4');
+        cy.get('#unidade_duracao').select('meses');
+        cy.get(':nth-child(2) > .atv > .input_number').type('10');
+        cy.get(':nth-child(2) > .atv > .select_atv').select('dias');
+        cy.get(':nth-child(3) > .atv > .input_number').type('1');
+        cy.get(':nth-child(3) > .atv > .select_atv').select('semanas');
+        cy.get('.registrar_cultura').click();
+        cy.wait(2000);
+        cy.get('[href="/verculturas/"]').click();  
+        cy.get('.editar-btn > .fas').first().click(); 
+        cy.wait(2000);
+        cy.get('.data-colheita')
+        .clear()
+        .type('2024-12-15')
+        .trigger('change');
+        cy.wait(2000);       
+        cy.get('.confirmar-edicao > .fas').first().click();
+        cy.wait(2000);
+    });
 
-  it('cenario4 - mudando nome da cultura; erro', () => { //  nao vai alterar foto
-    //login
-    cy.visit('http://127.0.0.1:8000/');
-    cy.get('#username').type('laviniaxs');
-    cy.get('#password').type('1234');
-    cy.get('.submit-button').click();
-    cy.wait(3000);
-   
-    cy.visit('http://127.0.0.1:8000/verculturas/');
-    cy.wait(1000);
+    
 
-    // editando cultura
-    cy.get('#cultura-20 > .editar-btn > .fas').click();
-    cy.get('#cultura-20 > .cultura-nome').clear();
-    cy.get('#cultura-20 > .cultura-nome').type('Alface'); 
-    cy.get('#cultura-20 > .confirmar-edicao > .fas').click();
-    cy.wait(5000);
-  });
-
-  it('cenario5 - mudando data colheita - 100%', () => {
-    //login
-    cy.visit('http://127.0.0.1:8000/');
-    cy.get('#username').type('laviniaxs');
-    cy.get('#password').type('1234');
-    cy.get('.submit-button').click();
-    cy.wait(3000);
-   
-    cy.visit('http://127.0.0.1:8000/verculturas/');
-    cy.wait(1000);
-
-    // editando cultura
-    cy.get('#cultura-20 > .editar-btn > .fas').click();
-    cy.wait(5000);
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-data-colheita > .data-colheita').click();
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-data-colheita > .data-colheita').clear();
-    cy.get('#cultura-20 > .hold_culturas-dados > .culturas-dados > .cultura-data-colheita > .data-colheita').type('2024-10-23');
-    cy.wait(5000);
-    cy.get('#cultura-20 > .confirmar-edicao > .fas').click();
-    cy.wait(5000);
-  });
-
+    
 });
--- cypress/e2e/historico.cy.js
+++ cypress/e2e/historico.cy.js
@@ -26,10 +26,24 @@ describe("teste 'meu histórico'", () => {
         cleanupAndSetupData(); // Limpa e prepara o banco de dados antes de cada teste
     });
 
-    it('cenario1 - buscar', () => {
+    it('cenario1 - buscar histórico', () => {
         cy.get('#username').type('cesar.school');
         cy.get('.password-wrapper').type('1234');
         cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('[href="/add/"]').click();
+        cy.get('.input_cultura').select('Tomate');
+        cy.get('[name="area"]').select('C');
+        cy.get('[name="linha"]').select('3');
+        cy.get('.container_loc-desc > .span > input').type('Sementes de Daniel');
+        cy.get('#data_plantio').type('2024-09-23');
+        cy.get('#duracao').type('4');
+        cy.get('#unidade_duracao').select('semanas');
+        cy.get(':nth-child(2) > .atv > .input_number').type('10');
+        cy.get(':nth-child(2) > .atv > .select_atv').select('dias');
+        cy.get(':nth-child(3) > .atv > .input_number').type('1');
+        cy.get(':nth-child(3) > .atv > .select_atv').select('semanas');
+        cy.get('.registrar_cultura').click();
         cy.wait(2000);
         cy.get('.user-icon > svg').click();
         cy.get('[href="/meuhistorico/"]').click();
@@ -37,7 +51,32 @@ describe("teste 'meu histórico'", () => {
         cy.wait(2000);
     });
 
-    it('cenario2 - dar logout', () => {
+    it('cenario2 - buscar histórico e não encontrar', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('[href="/add/"]').click();
+        cy.get('.input_cultura').select('Tomate');
+        cy.get('[name="area"]').select('C');
+        cy.get('[name="linha"]').select('3');
+        cy.get('.container_loc-desc > .span > input').type('Sementes de Daniel');
+        cy.get('#data_plantio').type('2024-09-23');
+        cy.get('#duracao').type('4');
+        cy.get('#unidade_duracao').select('semanas');
+        cy.get(':nth-child(2) > .atv > .input_number').type('10');
+        cy.get(':nth-child(2) > .atv > .select_atv').select('dias');
+        cy.get(':nth-child(3) > .atv > .input_number').type('1');
+        cy.get(':nth-child(3) > .atv > .select_atv').select('semanas');
+        cy.get('.registrar_cultura').click();
+        cy.wait(2000);
+        cy.get('.user-icon > svg').click();
+        cy.get('[href="/meuhistorico/"]').click();
+        cy.get('#searchInput').type("alface");
+        cy.wait(2000);
+    });
+
+    it('cenario3 - dar logout', () => {
         cy.get('#username').type('cesar.school');
         cy.get('.password-wrapper').type('1234');
         cy.get('.submit-button').click();
