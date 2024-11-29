11cfa410eab4a858f9f002017f2034c27487147b
brunooassuncao <bas@cesar.school>
2024-11-28 22:50:07 -0300

    Update add_gerenciar_lembretes.cy.js

--- cypress/e2e/add_gerenciar_lembretes.cy.js
+++ cypress/e2e/add_gerenciar_lembretes.cy.js
@@ -1,5 +1,5 @@
 describe("teste 'lembretes'", () => {
-     
+
     const cleanupAndSetupData = () => {
         cy.request('GET', 'http://127.0.0.1:8000/cleanup_db/').then(() => {
             cy.clearCookies(); // Limpa cookies
@@ -21,69 +21,86 @@ describe("teste 'lembretes'", () => {
         console.error('Erro não tratado detectado:', err);
         return false; // Evita falhas automáticas nos testes
     });
-    
+
     beforeEach(() => {
         cleanupAndSetupData(); // Limpa e prepara o banco de dados antes de cada teste
     });
 
-   it('cenario1 - adicionar', () => {
-       cy.get('#username').type('cesar.school');
-       cy.get('.password-wrapper').type('1234');
-       cy.get('.submit-button').click();
-       cy.wait(2000);
-       cy.get('.form_reminder > .reminder').type("irrigar alface");
-       cy.get('.reminder-plus').click();
-       cy.wait(2000);
-   });
+    it('cenario1 - adicionar', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('.form_reminder > .reminder').type("irrigar alface");
+        cy.get('.reminder-plus').click();
+        cy.wait(2000);
+    });
 
-   it('cenario2 - adicionar sem nada escrito', () => {
-       cy.get('#username').type('cesar.school');
-       cy.get('.password-wrapper').type('1234');
-       cy.get('.submit-button').click();
-       cy.wait(2000);
-       cy.get('.reminder-plus').click();
-       cy.wait(2000);
-   });
+    it('cenario2 - adicionar sem nada escrito', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('.reminder-plus').click();
+        cy.wait(2000);
+    });
 
-   it('cenario3 - adicionar e editar', () => {
-       cy.get('#username').type('cesar.school');
-       cy.get('.password-wrapper').type('1234');
-       cy.get('.submit-button').click();
-       cy.wait(2000);
-       cy.get('.form_reminder > .reminder').type("irrigar alface");
-       cy.get('.reminder-plus').click();
-       cy.get('.edit_button > img').first().click();
-       cy.wait(1000);
-       cy.get('input[id^="edit-text-"]').type(" e brocolis"); 
-       cy.wait(500);
-       cy.get('button[onclick^="saveEdit"]').click();
-       cy.wait(2000);
-});
+    it('cenario3 - adicionar e editar', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(1000);
 
-   it('cenario4 - adicionar e excluir', () => {
-       cy.get('#username').type('cesar.school');
-       cy.get('.password-wrapper').type('1234');
-       cy.get('.submit-button').click();
-       cy.wait(2000);
-       cy.get('.form_reminder > .reminder').type("irrigar alface");
-       cy.get('.reminder-plus').click();
-       cy.get('.edit_button > img').first().click();
-       cy.wait(1000);
-       cy.get('.fas').click();
-       cy.wait(2000);
-});
+        cy.get('.form_reminder > .reminder').type("adicionar");
+        cy.get('.reminder-plus').click();
+        cy.wait(1000);
 
-   it('cenario5 - adicionar e dar ok', () => {
-       cy.get('#username').type('cesar.school');
-       cy.get('.password-wrapper').type('1234');
-       cy.get('.submit-button').click();
-       cy.wait(2000);
-       cy.get('.form_reminder > .reminder').type("irrigar alface");
-       cy.get('.reminder-plus').click();
-       cy.wait(1000);
-        cy.get('[id^="reminder-checkbox-"]').first().click(); 
-       cy.wait(2000);
-});
+        cy.contains('li.reminder', 'adicionar').should('exist');
+
+        cy.get('.hold_reminders > .edit_button').click();
+
+        cy.contains('li.reminder', 'adicionar').within(() => {
+            cy.get('input[type="text"]').clear().type("adicionar alface");
+            cy.get('button').contains('Salvar').click();
+        });
+
+        cy.contains('li.reminder', 'adicionar alface').should('exist');
+    });
+
+
+    it('cenario4 - adicionar e excluir', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(1000);
+
+        cy.get('.form_reminder > .reminder').type("irrigar alface");
+        cy.get('.reminder-plus').click();
+        cy.wait(1000);
+
+        cy.contains('li.reminder', 'irrigar alface').should('exist');
+
+        cy.get('.hold_reminders > .edit_button').click();
+
+        cy.contains('li.reminder', 'irrigar alface').within(() => {
+            cy.get('button[title="Excluir"]').click();
+        });
+        cy.wait(2000);
+
+        cy.contains('li.reminder', 'irrigar alface').should('not.exist');
+    });
+
+    it('cenario5 - adicionar e dar ok', () => {
+        cy.get('#username').type('cesar.school');
+        cy.get('.password-wrapper').type('1234');
+        cy.get('.submit-button').click();
+        cy.wait(2000);
+        cy.get('.form_reminder > .reminder').type("irrigar alface");
+        cy.get('.reminder-plus').click();
+        cy.wait(1000);
+        cy.get('[id^="reminder-checkbox-"]').first().click();
+        cy.wait(2000);
+    });
 
 
 
